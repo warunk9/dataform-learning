16e38f4f1bacc91052d13f527c0d14fabd150ab5
Swati Thakur <thakurswati@google.com>
2023-08-14 18:41:06 +0000

    Products Silver Changes

---
+++ definitions/silver/products.js
@@ -0,0 +1,29 @@
+publish("products_sl",
+   {
+    type: "table",
+    database: dataform.projectConfig.vars.silverDatabase,
+    schema: dataform.projectConfig.vars.silver_Schema,
+    description: "Product Details"  ,
+    tags: ["products" ,"silver"],  
+    })
+    .query(ctx => `SELECT 
+    id,
+    round(cost,3) as cost,
+    nullif(trim(category),'') as category,
+    nullif(trim(name),'') as name,
+    nullif(trim(brand),'') as brand,
+    round(retail_price,3) as retail_price,
+    nullif(trim(department),'') as department,
+    nullif(trim(sku),'') as sku,
+    distribution_center_id
+    FROM ${ctx.ref("products")}`);
+
+assert("df_ecomm_products_sl_assertions_notnull")
+.query(ctx => `SELECT id FROM  ${ctx.ref("products")} WHERE id IS NULL`);
+
+
+
+
+
+
+

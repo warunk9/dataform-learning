5871136019c887d758075df7300f2d1729007479
Warun Kumar <warunk@google.com>
2023-08-15 20:38:55 +0000

    some good practice

---
+++ definitions/assertions/checkemail_users.js
@@ -0,0 +1,4 @@
+//assert("df_ecomm_users_sl_assertions_email").query(ctx => {
+//  return ctx.ref("check_func").emailsyntax(ctx.ref("stg_products"),"email");
+//});
+
--- definitions/includes/constants.js
+++ definitions/includes/constants.js
@@ -1,6 +1,8 @@
 const project_id = "gcp-sandbox-3-393305";
 const first_date = "'1970-01-01'";
+const base_year = "1900";
 module.exports = {
   project_id,
-  first_date
+  first_date,
+  base_year
 };

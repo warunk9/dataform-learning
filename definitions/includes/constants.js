420b569c99d67c318fe9e98faf70e1272ecf5b20
Warun Kumar <warunk@google.com>
2023-08-16 07:21:42 +0000

    silver changes

--- definitions/includes/constants.js
+++ definitions/includes/constants.js
@@ -1,8 +1,8 @@
-const project_id = "gcp-sandbox-3-393305";
-const first_date = "'1970-01-01'";
-const base_year = "1900";
+const PROJECT_ID = "gcp-sandbox-3-393305";
+const BASE_YEAR = "1900";
+const scd = require("dataform-scd");
 module.exports = {
-  project_id,
-  first_date,
-  base_year
+  PROJECT_ID,
+  BASE_YEAR,
+  scd
 };

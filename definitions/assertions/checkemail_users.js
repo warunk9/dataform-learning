71bf91f475b97d57f4c3e374b6bc60d64903b330
Warun Kumar <warunk@google.com>
2023-08-17 19:36:55 +0000

    functions

--- definitions/assertions/checkemail_users.js
+++ definitions/assertions/checkemail_users.js
@@ -1,4 +1,4 @@
 assert("df_ecomm_users_sl_assertions_email").query(ctx => {
-   `${check_func.emailsyntax(ctx.ref("stg_users"), "email")}`;
+   `${assert_utils.emailsyntax(ctx.ref("stg_users"), "email")}`;
 });
 

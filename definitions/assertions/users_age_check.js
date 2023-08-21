84227dadc7fdc3da29d38d25a0d0357174315ea0
Warun Kumar <warunk@google.com>
2023-08-21 08:49:19 +0000

    add js assert

--- definitions/assertions/users_age_check.js
+++ definitions/assertions/users_age_check.js
@@ -1,8 +1,8 @@
 
 
-assert("df_ecomm_users_sl_assertions_age").query(ctx => {
-   `${assert_utils.age_check(ctx.ref("stg_users"), "age")}`;
-});
+assert("df_ecomm_users_sl_assertions_age").query(ctx =>
+   assert_utils.age_check(ctx.ref("stg_users"), 'cast(age as INT64)')
+);
 
 
 

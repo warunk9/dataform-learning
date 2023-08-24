5ddd8f16dfb2c8ef0ee4daccc61d0984151a026f
Warun Kumar <warunk@google.com>
2023-08-24 06:10:13 +0000

    test scd2

---
+++ definitions/transformation/dim_users_scd.js
@@ -0,0 +1,32 @@
+
+const scd = require("dataform-scd");
+
+/**
+ * Create an SCD table on top of the table defined in declare_users.sqlx.
+ */
+const { updates, table } = scd("dim_users", {
+  // A unique identifier for rows in the table.
+  uniqueKey: "id",
+  // A field that stores a timestamp or date of when the row was last changed.
+  timestamp: "updated_at",
+  // The source table to build slowly changing dimensions from.
+  source: {
+    schema: dataform.projectConfig.vars.bronze_Schema,
+    name: "stg_users",
+  },
+  // Any tags that will be added to actions.
+  tags: ["scd","silver","dim_users"],
+  // Documentation of table columns
+  columns: {user_id: "User ID", some_field: "Data Field", updated_at: "Timestamp for updates"},
+  // Configuration parameters to apply to the incremental table that will be created.
+  incrementalConfig: {
+    bigquery: {
+      partitionBy: "updated_at",
+    },
+  },
+});
+
+// Additional customization of the created models can be done by using the returned actions objects.
+updates.config({
+  description: "Updates table for SCD",
+});

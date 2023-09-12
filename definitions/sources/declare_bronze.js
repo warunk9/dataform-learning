acd24bd09107469797819a23f264313b7016158a
Madhu Jain <madhujain@google.com>
2023-09-12 14:28:33 +0000

    order_item_fact changed to dim_order_item as this is SCD1 load type

--- definitions/sources/declare_bronze.js
+++ definitions/sources/declare_bronze.js
@@ -1,5 +1,5 @@
 
-const table = ["stg_products","stg_order_items","stg_orders"]
+const table = ["stg_products","stg_order_items","stg_orders","stg_inventory_items"]
 
 table.forEach(utils.declareFn)
 
